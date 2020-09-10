2b4686d09b213c5b016c4e3c4571a7bd7e5cedf1
Daniel Sanchez <67488770+danielwsanchez@users.noreply.github.com>
2020-09-09 22:42:03 -0400

    Add files via upload

---
+++ FlixApp/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  FlixApp
+//
+//  Created by Daniel Sanchez on 9/9/20.
+//  Copyright © 2020 Daniel Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
